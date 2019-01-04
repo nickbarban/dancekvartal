77e50c39e0ab7c8876691402753a62774c186060
nbarban <nbarban@millhouse.com>
2019-01-04 14:03:25 +0200

    remove ua language

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -80,7 +80,6 @@ module.exports = (options) => ({
         new MergeJsonWebpackPlugin({
             output: {
                 groupBy: [
-                    { pattern: "./src/main/webapp/i18n/ua/*.json", fileName: "./i18n/ua.json" },
                     { pattern: "./src/main/webapp/i18n/en/*.json", fileName: "./i18n/en.json" },
                     { pattern: "./src/main/webapp/i18n/ru/*.json", fileName: "./i18n/ru.json" }
                     // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
